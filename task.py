fb5600bf009330f3ed8dc3dc63aafa5d7703cf39
greenboxspb <green.box.spb@gmail.com>
2017-12-21 13:42:19 +0300

    task_17

--- task.py
+++ task.py
@@ -4,4 +4,6 @@
 '''
 
 numbers = input('Введите три числа через пробел: ').split(' ')
-print(numbers)
+average = (int(numbers[0]) + int(numbers[1]) + int(numbers[2])) / len(numbers)
+difference = ((int(numbers[0]) + int(numbers[1])) * 2) - (int(numbers[2]) * 3)
+print(round(average), difference)
