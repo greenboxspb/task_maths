9d8fc750618eea849a6fd9e3bd9c087833be6018
greenboxspb <green.box.spb@gmail.com>
2017-12-28 11:06:49 +0300

    task_12

--- task.py
+++ task.py
@@ -5,11 +5,11 @@
 import math as m
 
 x = 3.6
-step_1 = (m.e) ** (x -2)
-print(step_1)
+step_1 = (m.e) ** (x - 2)
 step_2 = m.fabs(m.sin(x))
-print(step_2)
 step_3 = (x ** 10) * m.cos(1/x)
 step_4 = step_1 + step_2
 step_5 = step_4 - step_3
 print(step_5)
+
+print(((m.e) ** (x - 2) + (m.fabs(m.sin(x)) - ((x ** 10) * m.cos(1/x)))))
