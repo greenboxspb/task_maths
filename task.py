0bcc77784f920dac47b5cd60c93c6c608a6983a1
greenboxspb <green.box.spb@gmail.com>
2017-12-28 08:04:14 +0300

    task_12

--- task.py
+++ task.py
@@ -1,21 +1,15 @@
 '''
-Вычислите значение выражения |x−5|−sinx/3 + √x^2+2014 * cos2x−3 при x=−2.34. Ответ: -1.76911
-'''
-
-'''
-|x−5|−sinx/3 + √x^2+2014 * cos2x−3 при x=−2.34. Ответ: -1.76911
+Вычислите значение выражения e^x−2+|sin(x)|−x^10*cos*1/x при x=3.6
 '''
 
 import math as m
 
-x = -2.34
-step_1 = abs(x - 5)
-step_2 = step_1 - m.sin(x)
-step_3 = step_2 / 3
-step_4 = m.sqrt((x ** 2) + 2014)
-step_5 = (step_4 * (m.cos(2 * x))) - 3
-step_6 = step_3 + step_5
-print(round(step_6, ndigits=5))
-
-print(round(((abs(x - 5) - m.sin(x)) / 3) + (((m.sqrt((x ** 2) + 2014)) * (m.cos(2 * x))) - 3), ndigits=5))
-
+x = 3.6
+step_1 = (m.e) ** (x -2)
+print(step_1)
+step_2 = m.fabs(m.sin(x))
+print(step_2)
+step_3 = (x ** 10) * m.cos(1/x)
+step_4 = step_1 + step_2
+step_5 = step_4 - step_3
+print(step_5)
