53ff7113d60169fd7720ea506cbfb12c11e5022d
greenboxspb <green.box.spb@gmail.com>
2018-01-12 12:18:05 +0300

    task_28

--- task.py
+++ task.py
@@ -2,3 +2,16 @@
 Пользователь вводит сумму вклада в банк и годовой процент.
 Найдите сумму вклада через 5 лет (рассмотреть два способа начисления процентов)
 '''
+
+money = int(input('Введите сумму вклада: '))
+percent = int(input('Введите процент: '))
+
+result_year = money + (((money / 100) * percent) * 5)
+print(result_year)
+
+result_month = money + ((((money / 100) * percent) / 12) * 60)
+print(result_month)
+
+# сложный процент "с ежегодной капитализацией процентов"
+res = money * ((1 + (percent / 100)) ** 5)
+print(res)
