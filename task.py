c9203aeecc79c45d4070b42c3cff11457e380afa
greenboxspb <green.box.spb@gmail.com>
2018-01-19 12:39:23 +0300

    task_33

--- task.py
+++ task.py
@@ -5,3 +5,16 @@
 
 import math as m
 
+x = int(input('Введите значение X: '))
+if x < 0:
+    print('Ошибка. Введите положительное число.')
+y = int(input('Введите значение Y: '))
+if y < 0:
+    print('Ошибка. Введите положительное число.')
+
+decision = m.sqrt(x - m.sqrt(y))
+if decision.is_integer() is True:
+    print(round(decision))
+
+else:
+    print(decision)
