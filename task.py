bde44765aaa6046575f554380ab0abd8c32b786b
greenboxspb <green.box.spb@gmail.com>
2018-03-17 11:05:59 +0300

    task_41

--- task.py
+++ task.py
@@ -3,9 +3,25 @@
 иначе увеличить его на 1.
 '''
 
+
 num = int(input('Введите значение: '))
 
-if num >= 100 or num <= -100:
-    print(num - num)
-else:
-    print(num + 1)
+'''
+Вариант 1
+'''
+# if num >= 100 or num <= -100:
+#     num = 0
+#     print(num)
+# else:
+#     print(num + 1)
+
+'''
+Вариант 2
+'''
+# num = 0 if num >= 100 or num <= -100 else num + 1
+# print(num)
+
+'''
+Вариант 3
+'''
+print(num + 1 if -100 <= num <= 100 else 0)
