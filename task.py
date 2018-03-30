a0f11689067079ddf178c45c3b81f415fc089b71
greenboxspb <green.box.spb@gmail.com>
2018-03-30 12:41:44 +0300

    task_48

--- task.py
+++ task.py
@@ -9,14 +9,11 @@ a, b, c = int(num[0]), int(num[1]), int(num[2])
 sum1, sum2, sum3 = a + b, a + c, b + c
 
 if sum1 > sum2 and sum1 > sum3:
-    print(a, b, '"1"')
+    print(a, b)
 elif sum2 > sum1 and sum2 > sum3:
-    print(a, c, '"2"')
+    print(a, c)
 elif sum3 > sum1 and sum3 > sum2:
-    print(b, c, '"3"')
-elif sum1 == sum2:
-    print('Суммы равны', '"4"')
-elif sum2 == sum3:
-    print('Суммы равны', '"5"')
-elif sum1 == sum3:
-    print('Суммы равны', '"6"')
+    print(b, c)
+else:
+    print("числа не были определены", '"4"')
+
