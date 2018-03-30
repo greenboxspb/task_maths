2b90b8a8241bb65f0ba4df4bacb922f7cdea5729
greenboxspb <green.box.spb@gmail.com>
2018-03-30 09:00:57 +0300

    task_48

--- task.py
+++ task.py
@@ -3,32 +3,20 @@
 Найдите те два из них, сумма которых наибольшая.
 '''
 
-a = 1
-b = 1
-c = 1
+num = input('Введите 3 числа через пробел: ').split(' ')
 
-# if a < b and a < c:
-#     print(b + c)
-# elif b < a and b < c:
-#     print(a + c)
-# elif c < a and c < b:
-#     print(a + b)
-
-
-
-sum1 = a + b
-sum2 = a + c
-sum3 = b + c
+a, b, c = int(num[0]), int(num[1]), int(num[2])
+sum1, sum2, sum3 = a + b, a + c, b + c
 
 if sum1 > sum2 and sum1 > sum3:
-    print(a + b, '1')
-if sum2 > sum1 and sum2 > sum3:
-    print(a + c, '2')
-if sum3 > sum1 and sum3 > sum2:
-    print(b + c, '3')
-if sum1 == sum2:
-    print(a + b, '4')
+    print(a + b, '"1"')
+elif sum2 > sum1 and sum2 > sum3:
+    print(a + c, '"2"')
+elif sum3 > sum1 and sum3 > sum2:
+    print(b + c, '"3"')
+elif sum1 == sum2:
+    print(sum1, '"4"')
 elif sum2 == sum3:
-    print(a + c, '5')
+    print(sum2, '"5"')
 elif sum1 == sum3:
-    print(a + b, '6')
+    print(sum3, '"6"')
