792cd81c2b03cbb4c7b155a379cb2459b63d52e9
greenboxspb <green.box.spb@gmail.com>
2018-04-02 20:40:47 +0300

    task_54

--- task.py
+++ task.py
@@ -6,15 +6,14 @@
 num = input('Введите три числа через пробел: ').split(' ')
 n1, n2, n3 = int(num[0]), int(num[1]), int(num[2])
 
-if not n1 == n2 == n3:
-    if n1 == n2 or n2 == n3 or n1 == n3:
-        if n1 < 5 and n2 < 5 and n3 < 5:
-            print('yes')
-        else:
-            print('no')
-    else:
-        print('no')
+if ((n1 < 5 and n2 < 5) or (n2 < 5 and n3 < 5) or (n1 < 5 and n3 < 5)) and not n1 == n2 == n3:
+    print('yes')
 else:
-    print('no')
+    print('no 3')
+
+
+
+
+
 
 
