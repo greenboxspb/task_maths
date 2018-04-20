622a6b7caae43c8e564af0cff869b5a2f4ce8d7e
greenboxspb <green.box.spb@gmail.com>
2018-04-20 10:56:52 +0300

    task_61

--- task.py
+++ task.py
@@ -13,3 +13,19 @@ elif number_3 == number_4:
     print('В числе есть одинаковые цифры')
 else:
     print('В числе НЕТ одинаковых цифр')
+
+
+'''
+Вариант № 2
+'''
+
+nums = input('Введите четырехзначное число: ')
+
+if int(nums[0]) == int(nums[1]) or int(nums[0]) == int(nums[2]) or int(nums[0]) == int(nums[3]):
+    print('В числе есть одинаковые цифры')
+elif int(nums[1]) == int(nums[2]) or int(nums[1]) == int(nums[3]):
+    print('В числе есть одинаковые цифры')
+elif int(nums[2]) == int(nums[3]):
+    print('В числе есть одинаковые цифры')
+else:
+    print('В числе НЕТ одинаковых цифр')
