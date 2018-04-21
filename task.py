9c874b9fa87cd9d7dab6b0075c4fca2fb6f1def5
greenboxspb <green.box.spb@gmail.com>
2018-04-21 11:49:51 +0300

    task_65

---
+++ task.py
@@ -0,0 +1,36 @@
+'''
+Дано четырехзначное число.
+Переставьте местами цифры так, чтобы сначала оказались цифры, меньшие пяти.
+'''
+
+'''
+если используем сортировку
+'''
+nums = list(input('Введите четырехзначное число: '))
+
+if int(nums[0]) < 5 or int(nums[1]) < 5 or int(nums[2]) < 5 or int(nums[3]) < 5:
+    nums.sort()
+print(''.join(nums))
+
+
+'''
+если используем копию списка и пустой список
+'''
+nums = list(input('Введите четырехзначное число: '))
+
+new_nums = nums.copy()
+nums_2 = []
+if int(nums[0]) < 5:
+    nums_2.append(nums[0])
+    new_nums.remove(nums[0])
+if int(nums[1]) < 5:
+    nums_2.append(nums[1])
+    new_nums.remove(nums[1])
+if int(nums[2]) < 5:
+    nums_2.append(nums[2])
+    new_nums.remove(nums[2])
+if int(nums[3]) < 5:
+    nums_2.append(nums[3])
+    new_nums.remove(nums[3])
+print(''.join(nums_2 + new_nums))
+
