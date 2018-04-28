e167f7c3518a291d0fb945bfdc549edf1df0e0ba
greenboxspb <green.box.spb@gmail.com>
2018-04-28 22:00:14 +0300

    task_77

--- task.py
+++ task.py
@@ -3,9 +3,23 @@
 Количество строк вводит пользователь, количество столбцов равно 5.
 '''
 
+'''
+через while
+'''
+
+# user_string = int(input('Введите количество строк: '))
+#
+# line = 0
+# while line < user_string:
+#     print('0' * 5)
+#     line += 1
+
+
+'''
+через for
+'''
+
 user_string = int(input('Введите количество строк: '))
 
-column = 0
-while column < 5:
-    print('0' * user_string)
-    column += 1
+for line in list(range(0, user_string)):
+    print('0' * 5)
