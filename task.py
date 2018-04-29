9631e3cdc5480668dbf3fa109af7b6a23325ad19
greenboxspb <green.box.spb@gmail.com>
2018-04-29 12:21:07 +0300

    task_78

--- task.py
+++ task.py
@@ -7,10 +7,23 @@
 (квадрат из n строк, в каждой строке n звездочек)
 '''
 
-user_line = int(input('Введите количество строк: '))
-symbol_asterisk = int(input('Введите количество звездочек: '))
+'''
+через while
+'''
 
-line = 0
-while line < user_line:
-    print('*' * symbol_asterisk)
-    line += 1
+# user_line = int(input('Введите количество строк: '))
+# symbol_asterisk = int(input('Введите количество звездочек: '))
+#
+# line = 0
+# while line < user_line:
+#     print('*' * symbol_asterisk)
+#     line += 1
+
+'''
+через for
+'''
+
+user_num = int(input('Введите значение: '))
+
+for new_line in range(user_num):
+    print('*' * user_num)
