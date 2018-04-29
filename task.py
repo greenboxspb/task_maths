14d3878c87dbfc7bb49efa0c2808684656c70bdc
greenboxspb <green.box.spb@gmail.com>
2018-04-29 13:06:44 +0300

    task_80

--- task.py
+++ task.py
@@ -2,6 +2,10 @@
 Вывести на экран ряд чисел 1001,  1004,  1007,  ... 1025.
 '''
 
+'''
+через while
+'''
+
 nums = 1001
 list_nums = []
 while nums < 1025:
@@ -9,3 +13,11 @@ while nums < 1025:
     nums += 3
 print(', '.join(list_nums) + '{0}'.format(', 1025.'))
 
+
+'''
+через for
+'''
+
+for nums in range(1001, 1025, 3):
+    print(nums, end=', ')
+print('1025.')
