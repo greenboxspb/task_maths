a65f6828438531812cf83628960b0d930dc349bd
greenboxspb <green.box.spb@gmail.com>
2018-04-29 13:09:06 +0300

    task_81

--- task.py
+++ task.py
@@ -2,9 +2,22 @@
 Вывести на экран числа 100, 96, 92, ... до последнего положительного включительно.
 '''
 
+'''
+через while
+'''
+
 nums = 100
 list_nums = []
 while nums > 0:
     list_nums.append(str(nums))
     nums -= 4
 print(', '.join(list_nums) + '{0}'.format(', 0.'))
+
+
+'''
+через for
+'''
+
+for nums in range(100, 0, -4):
+    print(nums, end=', ')
+print('0.')
