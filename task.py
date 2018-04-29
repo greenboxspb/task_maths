84762d7f2deceae4a6ca9554fb26c330e154613b
greenboxspb <green.box.spb@gmail.com>
2018-04-29 13:03:56 +0300

    task_79

--- task.py
+++ task.py
@@ -2,9 +2,21 @@
 Выведите на экран числа 1, 2, 3, 4, ..., 20.
 '''
 
-nums = 1
-list_nums = []
-while nums < 20:
-    list_nums.append(str(nums))
-    nums += 1
-print(', '.join(list_nums) + '{0}'.format(', 20.'))
+'''
+через while
+'''
+
+# nums = 1
+# list_nums = []
+# while nums < 20:
+#     list_nums.append(str(nums))
+#     nums += 1
+# print(', '.join(list_nums) + '{0}'.format(', 20.'))
+
+'''
+через for
+'''
+
+for nums in range(1, 20):
+    print(nums, end=', ')
+print('20.')
